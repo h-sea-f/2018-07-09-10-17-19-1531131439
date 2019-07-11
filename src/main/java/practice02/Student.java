d2167c9548c9c599c3c70e6c6ad6e55f8989c6a0
h-sea-f <h_sea_f@163.com>
2019-07-11 22:23:16 +0800

    finish 01,02,03,04

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,18 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    protected int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age, int klass){
+        this.klass=klass;
+        this.age=age;
+        this.name=name;
+    }
+    public String introduce(){
+        return "I am a Student. I am at Class "+klass+".";
+    }
 }
--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,19 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    protected int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age, int klass){
+        this.klass=klass;
+        this.age=age;
+        this.name=name;
+    }
+    public Student(){}
+    public String introduce(){
+        return "I am a Student. I am at Class "+klass+".";
+    }
 }
